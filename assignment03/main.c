4634447590e2fc786f3c64f957547c7ab30ab4ef
MrMobbi <julliatmathias06@gmail.com>
2025-07-10 12:29:10 +0200

    ass03 done

--- assignment03/main.c
+++ assignment03/main.c
@@ -5,20 +5,20 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mathias Julliat");
-MODULE_DESCRIPTION("Hello World!");
+MODULE_DESCRIPTION("norminette again");
 
 
-int	do_work(int *my_int, int retval) 
+static int	do_work(int *my_int, int retval) 
 {
 	int x;
 	int y = *my_int;
 	int z;
-	for (x = 0; x < my_int; ++x)
-	{
+
+	for (x = 0; x < y; ++x) {
 		udelay(10);
 	}
+
 	if (y < 10)
-		/* That was a long sleep, tell userspace about it */
 		pr_info("We slept a long time!");
 
 	z = x * y;
@@ -26,14 +26,14 @@ int	do_work(int *my_int, int retval)
 		return 1;
 }
 
-static int	__inti	my_init(void)
+static int	__init	my_init(void)
 {
 	int x = 10;
 	x = do_work(&x, x);
 	return x;
 }
 
-void	__exit	my_exit(void) {}
+static void	__exit	my_exit(void) {}
 
 module_init(my_init);
 module_exit(my_exit);
